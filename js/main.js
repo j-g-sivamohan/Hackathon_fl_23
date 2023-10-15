1e45d417c77e7d9fb85c25e4c7794a78accc7d32
Alice Mao <amao@wustl.edu>
2023-10-14 21:38:02 -0500

    added shit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var selectedDay = "M"
+var selectedTime= 9
+
+function setDay() {
+    selectedDay = d3.select("#selected-day").property("value")
+    selectedTime = d3.select("#time_range").property("value")
+    console.log(selectedDay + " " + selectedTime)
+
+    callQuery(selectedDay, selectedTime)
+}
